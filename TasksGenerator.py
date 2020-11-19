30b8cdf53f701f479f8cc4237f8cbdae4f9e270b
MaxouSenpai <32537195+MaxouSenpai@users.noreply.github.com>
2020-11-19 13:38:20 +0100

    Random working

--- TasksGenerator.py
+++ TasksGenerator.py
@@ -1,21 +1,40 @@
 import sys
+import random
 
 
 class TasksGenerator:
     """
     This class represents a generator of random synchronous systems of tasks with constrained deadlines.
     """
-    def __init__(self, utilisationFactor, tasksNumber):
+    def __init__(self, tasksNumber, period, utilisationFactor):
+        """
+        Construct the TasksGenerator.
+        :param tasksNumber: the number of tasks to generate
+        :param period: the range of the periods of the tasks
+        :param utilisationFactor: the range of the utilisation factors of the tasks
+        :param period: the range of the periods of the tasks
+        """
         self.utilisationFactor = utilisationFactor
+        self.period = period
         self.tasksNumber = tasksNumber
 
     def generate(self, outputFile):
         """
-        Generate a synchronous system of tasks with constrained deadlines.
+        Generate a random synchronous system of tasks with constrained deadlines.
         :param outputFile: the file on which the tasks have to be written
         """
-        tasks = [[0, 0, 0, 0] for _ in range(self.tasksNumber)]
-        # TODO random
+        tasks = []
+        for i in range(self.tasksNumber):
+            offset = 0
+            period = random.randint(self.period[0], self.period[1])
+            deadline = period
+            lowLimit = int(self.utilisationFactor[0] * period)
+            # The low limit should be rounded to the next integer
+            if (self.utilisationFactor[0] * period) % 10 != 0:
+                lowLimit += 1
+            upLimit = int(self.utilisationFactor[1] * period)
+            wcet = random.randint(lowLimit, upLimit)
+            tasks.append([offset, wcet, deadline, period])
         TasksGenerator.writeTasksToFile(tasks, outputFile)
 
     @staticmethod
@@ -30,15 +49,16 @@ class TasksGenerator:
                 file.write("{} {} {} {}\n".format(task[0], task[1], task[2], task[3]))
 
 
-def run(utilisationFactor, tasksNumber, outputFile):
+def run(tasksNumber, period, utilisationFactor, outputFile):
     """
     Run the generator with the specified options
-    :param utilisationFactor: the sum of the utilisation factors of all the tasks
     :param tasksNumber: the number of tasks to be generated
+    :param period: the range of the periods of the tasks
+    :param utilisationFactor: the range of the utilisation factors of the tasks
     :param outputFile: the file on which the tasks have to be written
     :return:
     """
-    tasksGenerator = TasksGenerator(utilisationFactor, tasksNumber)
+    tasksGenerator = TasksGenerator(tasksNumber, period, utilisationFactor)
     tasksGenerator.generate(outputFile)
     print("The tasks have successfully been generated and written to the output file")
 
@@ -47,29 +67,36 @@ def main():
     """
     Verify that the generator has all the required options and then launch it if so.
     """
-    # TasksGenerator -u float -n int -o output
-    if len(sys.argv) != 7:  # filename + options (3*2)
+    if len(sys.argv) != 9:  # pythonFilename + options (4*2)
         raise Exception("Not the right amount of arguments")
 
-    utilisationFactor, tasksNumber, outputFile = None, None, None
+    utilisationFactor, period, tasksNumber, outputFile = None, None, None, None
+    try:
+        for i in range(4):
+            option = sys.argv[i * 2 + 1]
+            value = sys.argv[i * 2 + 2]
 
-    for i in range(3):
-        option = sys.argv[i * 2 + 1]
-        value = sys.argv[i * 2 + 2]
+            if option == "-n":
+                tasksNumber = int(value)
 
-        if option == "-u":
-            utilisationFactor = float(value)
+            elif option == "-p":
+                lim = value.split(",")
+                period = (int(lim[0]), int(lim[1]))
 
-        elif option == "-n":
-            tasksNumber = int(value)
+            elif option == "-u":
+                lim = value.split(",")
+                utilisationFactor = (float(lim[0]), float(lim[1]))
 
-        elif option == "-o":
-            outputFile = value
+            elif option == "-o":
+                outputFile = value
 
-    if utilisationFactor is not None and tasksNumber is not None and outputFile is not None:
-        run(utilisationFactor, tasksNumber, outputFile)
-    else:
-        raise Exception("Mandatory option(s) not defined")
+        if tasksNumber is None or period is None or utilisationFactor is None or outputFile is None:
+            raise Exception("Mandatory option(s) not defined")
+
+        run(tasksNumber, period, utilisationFactor, outputFile)
+
+    except:
+        raise Exception("Problem(s) detected in the options")
 
 
 if __name__ == "__main__":
